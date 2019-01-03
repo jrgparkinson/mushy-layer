107c17492d72c377488d62e386496622726c6147
jparkinson <jamie.parkinson@gmail.com>
2019-01-03 15:50:12 +0000

    Refactoring how we plot the fixed chill figures so we can also make a movie

--- matlab/MethodsPaper/Fig7FixedChill.m
+++ matlab/MethodsPaper/Fig7FixedChill.m
@@ -4,14 +4,21 @@
 function Fig7FixedChill(output_dir, frames, figure_output_dir)
 
 if nargin < 1
-    output_dir = '/home/parkinsonjl/mnt/sharedStorage/Test/FixedChill-t100.0-Ra1e+07-Da5.0e-05-C5.00/Uniform-FixedChill-64--0/';
-    output_dir = '/home/parkinsonjl/mushy-layer/execSubcycle/amr/';
-    output_dir = '/home/parkinsonjl/mnt/sharedStorage/fixedChillAMR/TBottom1.3AMR4-2-0/';
+    
+    % Remote
+    %baseDir = '/network/group/aopp/oceans/AW002_PARKINSON_MUSH/';
+    
+    % Local
+    baseDir = '/home/parkinsonjl/mnt/sharedStorage/'; 
+    
+    output_dir = [baseDir, 'TestDiffusiveTimescale/FixedChill-t5.0e-02-Ra1e+06-Da4.8e-04-C2.00-Rel1.0e-04-0/'];
+    
 end
 
 if nargin < 2
     frames = [-1]; % just process the last frame by default
     frames = [3000, 4800, 17000]; % Testing
+    frames = [2288, 3874, 4576]; % Testing
 end
 
 if nargin < 3
@@ -31,7 +38,11 @@ end
 %     
 % end
 
- processSpecificFolder(output_dir, frames, figure_output_dir);
+% For png's, need times font I think
+set(groot, 'DefaultAxesFontName', 'times');
+set(groot, 'DefaultTextFontName', 'times');
+
+processSpecificFolder(output_dir, frames, figure_output_dir);
  
 end
 
@@ -41,7 +52,7 @@ function processSpecificFolder(output_dir, frames, figure_output_dir)
 
 
 savePNG = true;
-pcolorShadingInterp = true;
+
 close all; 
 
 domWidth = 1.0;
@@ -58,26 +69,21 @@ h = figure();
 h.Position = [200 200 plotScreenWidth+70 plotScreenHeight*length(frames)];
 
 % Common for all plots
-topFraction = plotHeight;
-fluidVelScale = 200;
-maxPsi = 20;
-minPsi = -maxPsi; 
-dPsi = (maxPsi-minPsi)/40; % need even number of divisions
-v = linspace(minPsi, maxPsi, 40);%minPsi+0.5*dPsi:dPsi:maxPsi-0.5*dPsi; % Avoid the zero contour
-SlLims = [-1.0, -0.0];
+options.pcolorShadingInterp = true;
+options.topFraction = plotHeight;
+options.fluidVelScale = 200;
+options.pcolorField = 'S'; %porosity, Sl
+options.timeTitle = false;
 
 % Setup axes based on number of plotfiles
 axPositions = {};
-allAxes = {};
 
-pcolorField = 'S'; %porosity, Sl
-
-numFrames = length(frames);
+options.numFrames = length(frames);
 pltWidth = 0.75;
-pltHeight = 0.8/numFrames;
+pltHeight = 0.8/options.numFrames;
 
 for i=1:length(frames)
-   axPositions{end+1} = [0.07, 0.06 + (numFrames-i)*(pltHeight+0.06), pltWidth, pltHeight];
+   axPositions{end+1} = [0.07, 0.06 + (options.numFrames-i)*(pltHeight+0.06), pltWidth, pltHeight];
 end
 
 % Get all frames in folder
@@ -85,412 +91,39 @@ end
 
 % Load plotfile
 for frame_i=1:length(frames)
-    axPos = axPositions{frame_i}; %[0.08 0.08 0.8 0.75];
-    
-    %ml = getFinalPlotFile(output_dir);
+    options.axPos = axPositions{frame_i}; %[0.08 0.08 0.8 0.75];
     thisFrame = frames(frame_i);
-    if thisFrame == -1
-        ml = getFinalPlotFile(output_dir);
-    else
-        ml =  MushyLayerOutput(2, thisFrame, output_dir, actual_plot_prefix, true);
-        
-    end
-    
-    
-    mlSmooth =  MushyLayerOutput(2, thisFrame, output_dir, actual_plot_prefix, true, 'bicubic');
-
-    if isempty(isprop(ml, 'levelArray'))
-        fprintf('Folder does not contain any plot files \n');
-        return;
-    end
-
-
-    % Get data to plot
-    [X,Y] = ml.grid();
-    porosity = ml.dataForComp(ml.components.Porosity).';
-    Sl = ml.dataForComp(ml.components.Liquidconcentration).';
-    S = ml.dataForComp(ml.components.Bulkconcentration).';
-    U = ml.dataForComp(ml.components.xDarcyvelocity).';
-    V = ml.dataForComp(ml.components.yDarcyvelocity).';
-    Streamfunction = ml.dataForComp(ml.components.streamfunction).';
-    
-    % Use level 1 data so the contours are smooth
-    lev1 = ml.levelArray(1);
-    Streamfunction1 = lev1.dataForComp(ml.components.streamfunction);
-    
-    U1 = lev1.dataForComp(ml.components.xDarcyvelocity);
-    V1 = lev1.dataForComp(ml.components.yDarcyvelocity);
-    [X1,Y1] = lev1.grid();
-    
-    porositySmooth = mlSmooth.dataForComp(ml.components.Porosity).';
-    [Xsmooth,Ysmooth] = mlSmooth.grid();
-    
-    
-    
-    % Now trim data
-    numypts = size(Y, 1);
-    ypts = round((1-topFraction)*numypts):1:numypts;
-    porosity = porosity(ypts, :);
-    Sl = Sl(ypts, :);
-    S = S(ypts, :);
-    U = U(ypts, :); V= V(ypts, :);
-    Streamfunction = Streamfunction(ypts, :);
-    X=X(ypts, :);
-    Y=Y(ypts, :);
-    
-    numypts_lev1 = size(Y1, 1);
-    yptslev1 = round((1-topFraction)*numypts_lev1):1:numypts_lev1;
-    Streamfunction1 = Streamfunction1(yptslev1, :);
-   % porosity1 =porosity1(yptslev1, :);
-    U1 =U1(yptslev1, :); V1=V1(yptslev1, :); X1=X1(yptslev1, :); Y1=Y1(yptslev1, :);
-    
-    numypts_smooth = size(Ysmooth, 1);
-    ypts_smooth = round((1-topFraction)*numypts_smooth):1:numypts_smooth;
-    porositySmooth = porositySmooth(ypts_smooth, :);
-    Xsmooth = Xsmooth(ypts_smooth, :);
-    Ysmooth = Ysmooth(ypts_smooth, :);
-   
-    % Get limits
-    xl = [X(1,1) X(end,end)];
-    yl = [Y(1,1) Y(end,end)];
-    dx = X(1,2) - X(1,1);
-    
-    hold on;
-
-    %allAxes{frame_i} = [];
-
-    if frame_i == 1
-          axPorosity{frame_i} = gca;
-    else
-          axPorosity{frame_i} = axes;
-    end
-  
-
-    % Porosity first
-    if strcmp(pcolorField, 'Sl')
-        pcolor(X,Y,Sl);
-        colormapLims = [-1, 0];
-        colormapLabel = 'S_l';
-        
-        colormap(axPorosity{frame_i}, bluewhitered(257));
-        
-        cmapTickLabels = {'-1.0', '0.0'};
-        cmapTicks = [-1, 0];
-        
-    elseif strcmp(pcolorField, 'porosity')
-        pcolor(X,Y,porosity);
-        
-        colormap(axPorosity{frame_i}, blues);
-        
-        colormapLims = [0, 1];
-        colormapLabel = '\chi';
-        cmapTickLabels = {'0 (solid)', '1 (liquid)'};
-        cmapTicks = [0 1];
-        
-    elseif strcmp(pcolorField, 'S')
-         %colormapLims = [-1.5 -0.5];
-        colormapLims = [-1.0, 1.0];
-        colormapLabel = 'Bulk Concentration, \Theta';
-        
-        cmapTickLabels = {sprintf('%1.1f', colormapLims(1)-1), sprintf('%1.1f',colormapLims(2)-1)};
-        cmapTicks = colormapLims;
-        
-        %minS = -2.0;
-        %deltaS = -2*(minS+1.0);
-        %deltaS = max(max(S)) - minS;
-        pcolor(X,Y,S+1);
-        
-        caxis(colormapLims);
-        
-        % or coolwarmcmap ?
-        colormap(axPorosity{frame_i}, bluewhitered(257));
-        
-    end
-    
-    if pcolorShadingInterp
-    shading interp;
-    end
-    
-  %  caxis(colormapLims);
-    
-    
-    if frame_i == 2
-        cbar = colorbar('Location', 'eastoutside');
-    end
-    
-    %caxis(colormapLims);
-    
-
-    daspect([ 1 1 1]);
-    xlim(xl);
-    ylim(yl);
-    box on;
-
-
-    allAxes{frame_i} = [axPorosity{frame_i}];
-
-    plotFlow = false;
-    if plotFlow
-        axQuiver = axes;
-
-        % Modify data a little to make velocities more similar
-        Uplot = sign(U).*sqrt(abs(U));
-        Vplot = sign(V).*sqrt(abs(V));
-
-        sx = 1:6:size(X, 1);
-        sy = 1:6:size(X, 2);
-        quiverc(X(sx,sy),Y(sx,sy),Uplot(sx,sy),Vplot(sx,sy), 0.8); 
-
-        axQuiver.Visible = 'off';
-
-        daspect([ 1 1 1]);
-        xlim(xl);
-        ylim(yl);
-
-        allAxes{frame_i}(end+1) = axQuiver;
-    end
-    
-    hold off;
-    
-    plotPorosityContours = true;
-    if plotPorosityContours
-        axPorosityContours{frame_i} = axes;
-        %colormap(axPorosityContours{frame_i}, autumn);
-       
-        v = 0:0.2:0.99;
-        
-       [M, c] = contour(Xsmooth,Ysmooth,porositySmooth, v);
-       c.LineWidth = 1;
-       c.LineColor = 'k';
-       axPorosityContours{frame_i}.Visible = 'off';
-         
-        daspect([1 1 1]);
-        xlim(xl);
-        ylim(yl);
-        
-        allAxes{frame_i}(end+1) = axPorosityContours{frame_i};
-    end
-    
-    plotStreamlines = false;
-    if plotStreamlines
-        axStream{frame_i} = axes;
-
-        % Modify data a little to make velocities more similar
-
-        %maxPsi = max(max(abs(Streamfunction)));
-       % minPsi = -maxPsi; 
-
-       % dPsi = (maxPsi-minPsi)/10; % need even number of divisions
-       % v = minPsi+0.5*dPsi:dPsi:maxPsi-0.5*dPsi; % Avoid the zero contour
-
-        colormap(axStream{frame_i}, autumn);
-        if frame_i == 2
-            cVel = streamfunctioncolor(X1,Y1,U1,V1,Streamfunction1,v, fluidVelScale);
-        %cbar.Label.String = 'Scaled fluid velocity';
-        %cbar.Ticks = [0 1];
-        else
-             streamfunctioncolor(X1,Y1,U1,V1,Streamfunction1,v, fluidVelScale);
-        end
-
-        axStream{frame_i}.Visible = 'off';
-
-        daspect([1 1 1]);
-        xlim(xl);
-        ylim(yl);
-
-        allAxes{frame_i}(end+1) = axStream{frame_i};
-
-    end
-
-    % Now liquid salinity contours
-    plotSl = false;
-    if plotSl
-        axSl = axes;
-        contour(Xbase,Ybase,Sl_base);
-        %pcolor(Xbase,Ybase,Sl_base);
-        %xlim(xlbase);
-        %ylim(ylbase);
-
-        v = -1.5:0.2:-0.05;
-
-        contour(X,Y,Sl_smooth, v);
-        xlim(xl);
-        ylim(yl);
-
-        axSl.Visible='off';
-        daspect([1 1 1]);
-
-        %cSl = colorbar();
-        allAxes{frame_i}(end+1) = axSl;
-    end
-
-
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Level outlines
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-   
-    if length(ml.levelArray) > 1
-        
-        axPolyshapes = allAxes{frame_i}(end);
-        
-        hold on;
-
-         if exist('polyshape')
-
-        domainBox = polyshape([xl(1) xl(1) xl(end) xl(end)], [yl(1) yl(end) yl(end) yl(1)]);
-
-        meshes = ml.getMeshes(domainBox);
-
-        % Plot meshes 
-        pshape = meshes(2);
-        pshape = trimShape(pshape);
-        
-        mesh2 = plot(axPolyshapes, pshape); % meshes on chombo level 1
-
-        %plot(domainBox);
-        mesh2.FaceAlpha = 0;
-        mesh2.EdgeColor = [1 0 1]; % magenta
-        mesh2.LineWidth = 2;
-
-        if length(meshes) > 2
-            pshape = meshes(3);
-            pshape = trimShape(pshape);
-            
-            mesh3 = plot(axPolyshapes, pshape); % meshes on chombo level 2
-            mesh3.FaceAlpha = 0;
-            mesh3.EdgeColor = [0 1 1]; %cyan
-            mesh3.LineWidth = 2;
-        end
-        
-        else
-        fprintf('Not plotting level outlines as polyshape cannot be found \n');
-         end
-    
-        %axPolyshapes.Visible='off';
-        %daspect([1 1 1]);
-        %allAxes{frame_i}(end+1) = axPolyshapes;
-        
-        hold off;
-        
-    end
-   
-
-    %axSl.Position = axPos;
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Colorbars
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if frame_i == 2
-        cbar.Label.String = colormapLabel;
-        cbar.Label.Position(1) = cbar.Label.Position(1) -1.0;
-        
-        cbar.Position(2) = cbar.Position(2)-0.1;
-        cbar.Position(4) = cbar.Position(4)+0.2;
-        %cPorosity.Label.Position = [0.5 1.2];
-        %cPorosity.Label.Rotation = 0;
-        cbar.Ticks = cmapTicks;
-        cbar.Limits = colormapLims;
-        
-        cbar.TickLabels = cmapTickLabels;
-    end
-%     elseif frame_i == 2
-%     
-%         cVel.Label.String = 'Fluid velocity';
-%         cVel.Ticks = [0 1.0];
-%        % maxU = sqrt(max(max(abs(U.^2 + V.^2))));
-%         cVel.TickLabels = {'0', sprintf('%.1e', fluidVelScale)};
-%         cVel.Label.Position = [1.0 0.5];
-%     
-%     end
-
-    %cPorosity.Label.Rotation = 0;
-        %cSl.Label.String = 'S_l';
-    %cSl.Position = [0.85 cSl.Position(2) cSl.Position(3) cSl.Position(4)];
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Link axes
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for i = 2:length(allAxes{frame_i})
-        linkaxes([allAxes{frame_i}(1), allAxes{frame_i}(i)]);
-    end
-
-    set(allAxes{frame_i}(1), 'Layer', 'top');
-
-
-    for i = 1:length(allAxes{frame_i})
-        allAxes{frame_i}(i).Position = axPos;
-    end
-
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Axis labels
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  %  box on;
-
-    allAxes{frame_i}(1).Visible = 'on';
-    axLabels{frame_i} = allAxes{frame_i}(1);
-
-    %daspect([ 1 1 1]);
-
-    %xlim(xl);
-    %ylim(yl);
-     format = '%1.1f';
-     
- 
-    
-    if frame_i == numFrames
-
-     xlab = xlabel(allAxes{frame_i}(1), '$x$');
-     xlab.Position(2) = xlab.Position(2) + 0.05;
-     axLabels{frame_i}.XTick = xl;
-     axLabels{frame_i}.XTickLabels = {sprintf(format, xl(1)-dx/4), sprintf(format, xl(2)+dx/4)};
-    
-    else
-        axLabels{frame_i}.XTick = [];
-    end
-    
-    ylab{frame_i} = ylabel(allAxes{frame_i}(1), '$z$');
-    ylab{frame_i}.Position(1) = ylab{frame_i}.Position(1) + 0.05;
-    axLabels{frame_i}.YTick = yl;
-
-    axLabels{frame_i}.YTickLabels = {sprintf(format, yl(1)-dx/4), sprintf(format, yl(2)+dx/4)};
-    
-    
-    
-    % Add a/b/c and time labels
     
     textLabels = {'(a)', '(b)', '(c)'};
-    thisLabel = [textLabels{frame_i}, ' $t = ',sprintf('%1.4f', ml.t) , '$'];
-    text(0.02, 0.55, thisLabel, 'FontSize', 16);
+    options.subPlotLabel = textLabels{frame_i};
     
-end    
-
-    % h = gcf;
-    h.InvertHardcopy = 'off';
-    h.Color = 'white';
-    set(h,'Units','Inches');
-    pos = get(h,'Position');
-    set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-    filename = fullfile(figure_output_dir, 'Fig7FixedChillSimulation');
+    Fig7PlotFrame(options, axPos, output_dir, actual_plot_prefix, thisFrame, frame_i);
     
-    if pcolorShadingInterp
-        filename = [filename, '-shadingInterp'];
-    else
-        filename = [filename, '-noShadingInterp'];
-    end
+end
 
-    %%if savePDF
-      % print(h,[filename, '.pdf'],'-dpdf','-r0')
-    % end
+% h = gcf;
+h.InvertHardcopy = 'off';
+h.Color = 'white';
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+filename = fullfile(figure_output_dir, 'Fig7FixedChillSimulation');
 
-    if savePNG
-        print(h,[filename, '.png'],'-dpng','-r800')
-    end
+if options.pcolorShadingInterp
+    filename = [filename, '-shadingInterp'];
+else
+    filename = [filename, '-noShadingInterp'];
+end
 
-    
+%%if savePDF
+% print(h,[filename, '.pdf'],'-dpdf','-r0')
+% end
+
+if savePNG
+    print(h,[filename, '.png'],'-dpng','-r800')
+end
+
+
 
     
 end
@@ -510,3 +143,5 @@ end
 stop =0;
 
 end
+
+
---
+++ matlab/MethodsPaper/Fig7MakeVideo.m
@@ -0,0 +1,202 @@
+function makeVideo(folder)
+
+set(0, 'defaultlinelinewidth',2);
+set(0, 'defaultaxeslinewidth',2);
+set(0, 'defaultpatchlinewidth',2);
+set(0, 'defaultAxesFontSize',20);
+set(groot, 'DefaultTextInterpreter', 'latex')
+set(groot, 'DefaultLegendInterpreter', 'latex')
+set(groot,'DefaultSurfaceEdgeColor','none') % removes edges from pcolor plots
+set(groot, 'DefaultAxesFontName', 'times');
+set(groot, 'DefaultTextFontName', 'times');
+set(groot, 'defaultAxesFontName', 'times');
+set(groot, 'defaultTextFontName', 'times');
+set(0, 'defaultAxesFontName', 'times');
+set(0, 'defaultTextFontName', 'times');
+
+if nargin < 1
+    
+    % Remote
+    baseFolder = '/network/group/aopp/oceans/AW002_PARKINSON_MUSH/';
+    
+    % local
+    baseFolder = '/home/parkinsonjl/mnt/sharedStorage/'; 
+    folder= [baseFolder, 'TestDiffusiveTimescale/FixedChill-t5.0e-02-Ra1e+06-Da4.8e-04-C2.00-Rel1.0e-04-0/'];
+end
+
+framePrefix = 'frame';
+plot_prefix = 'plt';
+
+videoFolder = fullfile(folder, 'video');
+if exist(videoFolder, 'dir') ~= 7
+    fprintf('Making directory %s \n', videoFolder);
+    mkdir(videoFolder);
+end
+
+testing = true;
+
+makeFrameFunction = @(folder, plotFile, options) fakeHeleShawFrame(folder, plotFile, options);
+options.SlMax = 0.1;
+options.axPos = [0.15 0.15 0.65 0.75];
+options.topFraction = 0.5;
+options.pcolorShadingInterp = true;
+options.fluidVelScale = 200;
+options.numFrames = 1;
+options.pcolorField = 'S'; %porosity, Sl
+options.subPlotLabel = ''; % no a/b/c/ label
+options.timeTitle = true;
+options.cbarPos = [];
+
+figPos = [100 100 900 400];
+
+close all;
+
+
+% 1) make frames if they don't exist
+plotFiles = dir(fullfile(folder, [plot_prefix, '*']));
+
+filesToDo = 1:length(plotFiles);
+%filesToDo = 23:23; %testing
+if testing
+    filesToDo = 1:20;
+end
+
+frameList = [];
+
+% Start timer
+tic;
+
+for i=filesToDo
+
+try
+    
+     plotFile = plotFiles(i).name;
+    
+    regexStr = ['[^\d]+(\d+)\.2d\.hdf5'];
+    [tokens,matches] = regexp(plotFile,regexStr,'tokens','match');
+    
+    thisFrame = str2num(tokens{1}{1});
+    frameList(i) = thisFrame;
+    
+    close all;
+    
+    frame  = -1;
+    prefix = '';
+    
+    saveFile = fullfile(videoFolder, [framePrefix, num2str(frameList(i)) ]);
+    
+    if exist([saveFile, '.png'], 'file') == 2
+        fprintf('frame %d exists \n', i);
+        %todo - uncomment line below
+        continue
+    else
+        fprintf('Making frame %d \n', i);
+    end
+    
+    h = figure();
+    
+    set(h, 'Position', figPos);
+    fprintf('Setting figure position: [%d %d %d %d]\n', figPos(1), figPos(2), figPos(3), figPos(4));
+      
+    %success = makeFrameFunction(folder, plotFile, options);
+    
+    success = Fig7PlotFrame(options, folder, plot_prefix, thisFrame, 1); 
+    
+    % pause(0.5);
+    if success
+        % Set position again to be sure
+        set(h, 'Position', figPos);
+        fprintf('Setting figure position: [%d %d %d %d]\n', figPos(1), figPos(2), figPos(3), figPos(4));
+        
+        
+        % Make sure we specify the paper position
+        paperPos =  [0 0 figPos(3)/100 figPos(4)/100];
+        set(h, 'PaperUnits', 'inches', 'PaperPosition', paperPos);
+        h.InvertHardcopy = 'off';
+        h.Color = 'white';
+        print(h,[saveFile, '.png'],'-dpng','-r200')
+        
+        fprintf('Saved to %s \n', saveFile);
+        
+    end
+    
+    elapsedTime = toc;
+    timePerRun = elapsedTime/i;
+    timeLeft = (length(filesToDo)-i)/timePerRun;
+    fprintf('Elapsed time: %f, predicted remaining time: %f \n', elapsedTime, timeLeft);
+    
+   catch e
+   fprintf('Exception \n ');
+   fprintf('%s', e.message);
+   end
+   
+end
+
+
+% Now compile frames into one video
+%frameList=filesToDo;
+
+frameSpacing = 2;
+fps = 32;
+video_name = sprintf('video-%dfps-%dframeSkip.avi', fps, frameSpacing);
+outputVideo = VideoWriter(fullfile(videoFolder, video_name));
+outputVideo.FrameRate = fps; 
+open(outputVideo);
+
+
+
+firstImage = imread(fullfile(videoFolder, sprintf('frame%d.png', frameList(1) ) ) );
+imgSize = size(firstImage);
+
+
+% Skip first 10 frames
+framesToMakeVideo = 1:frameSpacing:length(frameList);
+%if testing
+%framesToMakeVideo = 400:410;
+%end
+
+for i = framesToMakeVideo
+    fprintf('Making video, frame %d/%d \n', i, length(frameList));
+    
+    frame = frameList(i);
+    frameFile = sprintf('frame%d.png', frame);
+    fullFilename = fullfile(folder, frameFile);
+    if exist(fullFilename, 'file') == 2
+        
+        img = imread(fullFilename);
+        
+        % Trim size:
+        %img = img(1:880, 1:3100, :);
+        imgSize = size(img);
+        
+        fprintf('Image size: %dx%d \n', imgSize(1), imgSize(2));
+        
+        % if sum(size(img) == imgSize) < 3
+        %     fprintf('!!Skipping - wrong size frame!! \n');
+        %     continue
+        % end;
+        
+        
+        writeVideo(outputVideo, img)
+        
+    else
+        fprintf('Frame does not exist');
+    end
+    
+end
+
+fprintf('Writing video \n');
+close(outputVideo)
+
+ elapsedTime = toc;
+ 
+fprintf('Wrote video, script took %1.1f seconds.', elapsedTime );
+
+
+
+
+end
+
+
+
+
