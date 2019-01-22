95c6d018389442e73699bf7369f38779f60b7e74
jparkinson <jamie.parkinson@gmail.com>
2019-01-22 19:31:17 +0000

    Add post process command option, and reformat blank spaces

--- test/SlurmTask.py
+++ test/SlurmTask.py
@@ -4,21 +4,23 @@ import time
 import subprocess
 import re
 
-class SlurmTask:
 
+class SlurmTask:
     MAX_TASKS_PER_NODE = 16
-    MAX_MEMORY = 128000.0 # MB
-    
-    def __init__(self, folder, jobname, execFile, num_proc=1, timeLimit = 7.0, memoryLimit=4000, numNodes=0, mpirun=True):
+    MAX_MEMORY = 128000.0  # MB
+
+    postprocessCommand = ''
+
+    def __init__(self, folder, jobname, execFile, num_proc=1, timeLimit=7.0, memoryLimit=4000, numNodes=0, mpirun=True):
 
         self.jobname = jobname
         self.folder = folder
         self.num_proc = num_proc
-        
+
         self.execFile = execFile
-        self.timeLimit = timeLimit # Measured in days
-        self.memoryLimit = memoryLimit # in MB
-        self.mpirun=mpirun
+        self.timeLimit = timeLimit  # Measured in days
+        self.memoryLimit = memoryLimit  # in MB
+        self.mpirun = mpirun
 
         self.preprocessCommand = ''
         self.dependency = []
@@ -37,7 +39,7 @@ class SlurmTask:
             self.num_nodes = numNodes
         else:
             # Determine number of nodes based on number of processors and MAX_TASKS_PER_NODE
-            self.num_nodes = math.ceil(float(self.num_proc)/float(self.MAX_TASKS_PER_NODE))
+            self.num_nodes = math.ceil(float(self.num_proc) / float(self.MAX_TASKS_PER_NODE))
 
         # Distribute tasks onto nodes
         tasks_per_node, remainder = divmod(self.num_proc, self.num_nodes)
@@ -50,15 +52,19 @@ class SlurmTask:
             print('SlurmTask - Warning - number of processors doesn\'t split evenly onto number of nodes. \n')
 
         # Each node usually has two sockets
-        self.tasks_per_socket = math.ceil(self.tasks_per_node/2) #self.tasks_per_node #
+        self.tasks_per_socket = math.ceil(self.tasks_per_node / 2)  # self.tasks_per_node #
 
         # Make sure tasks per node * memory < MAX_MEMORY
-        if self.tasks_per_node*self.memoryLimit > self.MAX_MEMORY:
-            self.memoryLimit = 0.9*self.MAX_MEMORY/float(self.tasks_per_node)
-            print('Reducing memory limit to %1.0f GB per node to avoid exceeding node memory limit. \n' % (self.memoryLimit/1000))
+        if self.tasks_per_node * self.memoryLimit > self.MAX_MEMORY:
+            self.memoryLimit = 0.9 * self.MAX_MEMORY / float(self.tasks_per_node)
+            print('Reducing memory limit to %1.0f GB per node to avoid exceeding node memory limit. \n' % (
+                        self.memoryLimit / 1000))
 
     def setPreprocess(self, cmd):
         self.preprocessCommand = cmd
+
+    def setPostProcess(self, cmd):
+        self.postprocessCommand = cmd
 
     def setDependency(self, dep):
         if isinstance(dep, list):
@@ -66,23 +72,21 @@ class SlurmTask:
         else:
             self.dependency = [str(dep)]
 
-
     def setCustomCommand(self, cmd):
         self.customCommand = cmd
 
-    def writeSlurmFile(self, runFileName = 'run.sh', inputsFileName = 'inputs'):
-    	
-        #self.execFile = self.execFile.replace('OPT.', '')
-        
+    def writeSlurmFile(self, runFileName='run.sh', inputsFileName='inputs'):
 
-    	fh = open(self.getRunFile(runFileName), 'w+')
+        # self.execFile = self.execFile.replace('OPT.', '')
 
-        days,  remainder = divmod(self.timeLimit, 1)
-        hours, remainder = divmod(remainder*24, 1)
-        mins,  remainder = divmod(remainder*60, 1)
-        secs = remainder*60
+        fh = open(self.getRunFile(runFileName), 'w+')
 
-    	time_string = '%d-%d:%d:%d' % (days,hours,mins,secs)
+        days, remainder = divmod(self.timeLimit, 1)
+        hours, remainder = divmod(remainder * 24, 1)
+        mins, remainder = divmod(remainder * 60, 1)
+        secs = remainder * 60
+
+        time_string = '%d-%d:%d:%d' % (days, hours, mins, secs)
 
         mpiStr = ''
         if self.mpirun:
@@ -94,44 +98,51 @@ class SlurmTask:
         else:
             depStr = ''
 
-    	file_contents = ['#!/bin/bash \n',
-		'# Set your minimum acceptable walltime, format: day-hours:minutes:seconds \n',
-		'#SBATCH --time=' + time_string + '\n',
-		'#SBATCH --partition=shared,priority-ocean' + '\n',
-		'# Set name of job shown in squeue' + '\n',
-		'#SBATCH --job-name ' + self.jobname + '\n',
-		'# Request CPU resources' + '\n',
-		'#SBATCH --ntasks=' + str(int(self.num_proc) ) + '                  # Number of MPI ranks' + '\n',
-		'#SBATCH --cpus-per-task=' + str(int(self.cpu_per_task) ) + '            # Number of cores per MPI rank ' + '\n',
-		'#SBATCH --nodes=' + str(int(self.num_nodes)) + '                    # Number of nodes' + '\n',
-		'#SBATCH --ntasks-per-node=' + str(int(self.tasks_per_node) ) + '         # How many tasks on each node' + '\n',
-		'#SBATCH --ntasks-per-socket=' + str(int(self.tasks_per_socket) ) + '        # How many tasks on each CPU or socket (not sure what this really means)' + '\n',
-		'#SBATCH --distribution=cyclic:cyclic # Distribute tasks cyclically on nodes and sockets' + '\n',
-		'# Memory usage (MB)' + '\n',
-		'#SBATCH --mem-per-cpu=' + str(self.memoryLimit) + '\n',
-		'#SBATCH --output=' + os.path.join(self.folder, 'sbatch.out') + '   # Standard output and error log' + '\n',
-        depStr, 
-		'cd ' + self.folder +  '; \n \n ']
+        file_contents = ['#!/bin/bash \n',
+                         '# Set your minimum acceptable walltime, format: day-hours:minutes:seconds \n',
+                         '#SBATCH --time=' + time_string + '\n',
+                         '#SBATCH --partition=shared,priority-ocean' + '\n',
+                         '# Set name of job shown in squeue' + '\n',
+                         '#SBATCH --job-name ' + self.jobname + '\n',
+                         '# Request CPU resources' + '\n',
+                         '#SBATCH --ntasks=' + str(
+                             int(self.num_proc)) + '                  # Number of MPI ranks' + '\n',
+                         '#SBATCH --cpus-per-task=' + str(
+                             int(self.cpu_per_task)) + '            # Number of cores per MPI rank ' + '\n',
+                         '#SBATCH --nodes=' + str(int(self.num_nodes)) + '                    # Number of nodes' + '\n',
+                         '#SBATCH --ntasks-per-node=' + str(
+                             int(self.tasks_per_node)) + '         # How many tasks on each node' + '\n',
+                         '#SBATCH --ntasks-per-socket=' + str(int(
+                             self.tasks_per_socket)) + '        # How many tasks on each CPU or socket (not sure what this really means)' + '\n',
+                         '#SBATCH --distribution=cyclic:cyclic # Distribute tasks cyclically on nodes and sockets' + '\n',
+                         '# Memory usage (MB)' + '\n',
+                         '#SBATCH --mem-per-cpu=' + str(self.memoryLimit) + '\n',
+                         '#SBATCH --output=' + os.path.join(self.folder,
+                                                            'sbatch.out') + '   # Standard output and error log' + '\n',
+                         depStr,
+                         'cd ' + self.folder + '; \n \n ']
 
         if self.customCommand:
             file_contents.append(self.customCommand)
 
         else:
-            file_contents.append(self.preprocessCommand + ' ' + mpiStr + ' ' + self.execFile + ' ' + os.path.join(self.folder, inputsFileName))
-		
+            file_contents.append(
 
+        self.preprocessCommand + ' ' + mpiStr + ' ' + self.execFile + ' ' + os.path.join(self.folder, inputsFileName))
 
-    	fh.writelines(file_contents)
+        file_contents.append('\n' + self.postprocessCommand + '\n')
 
-    	fh.close()
+        fh.writelines(file_contents)
+
+        fh.close()
 
 
     def runTask(self, runFileName='run.sh'):
-    	cmd = 'cd ' + self.folder + ' ; sbatch ' + self.getRunFile(runFileName)
-    	
-        #os.system(cmd)
-        #result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        result = subprocess.check_output(    cmd,  stderr=subprocess.STDOUT,  shell=True)
+        cmd = 'cd ' + self.folder + ' ; sbatch ' + self.getRunFile(runFileName)
+
+        # os.system(cmd)
+        # result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
 
         res_str = result
 
@@ -140,8 +151,8 @@ class SlurmTask:
         # Submitted batch job 223086
         res = re.findall('Submitted batch job (\d+)', res_str)
 
-        #print(res_str)
-        #print(res)
+        # print(res_str)
+        # print(res)
 
         if res:
             self.jobID = int(res[0])
@@ -154,7 +165,6 @@ class SlurmTask:
         # Pause briefly in case submitting lots of slurm jobs
         time.sleep(0.5)
 
+
     def getRunFile(self, runFileName):
         return os.path.join(self.folder, runFileName)
-
-   
