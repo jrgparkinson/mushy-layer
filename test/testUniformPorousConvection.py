ea086fca5071ec226bb0ad4df371e70a0bff44cf
jparkinson <jamie.parkinson@gmail.com>
2019-02-27 21:06:06 +0000

    Change default options

--- test/testUniformPorousConvection.py
+++ test/testUniformPorousConvection.py
@@ -33,11 +33,11 @@ def uniform_porous_resolution_specific_params(nz_coarse, ref_rat, max_level, max
 
 def test_uniform_porous_convection(argv):
     # Default vals:
-    cfl = 0.2
+    cfl = 0.1
     nz_uniform = 128 # 128
     nz_vm = 64
     chi = 0.4
-    bc_accuracy = 1
+    bc_accuracy = 2
     u_del_u_method = 0
     advection_method = 1
 
