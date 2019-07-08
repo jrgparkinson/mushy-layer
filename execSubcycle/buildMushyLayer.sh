ccaeb4ad0a9c9bbe412e943b688cb6315af893fb
jrgparkinson <jamie.parkinson@gmail.com>
2019-07-08 10:15:55 +0100

    Updated build script

--- execSubcycle/buildMushyLayer.sh
+++ execSubcycle/buildMushyLayer.sh
@@ -13,16 +13,20 @@
 module unload gcc/5.2.0
 module load gcc/4.8.3
 
+# Dimensions to build in (usually 2)
+D=2
+echo "Build with DIM=$D"
+
 #cd ~/mushy-layer/execSubcycle/
 cd $MUSHY_LAYER_DIR/execSubcycle/
-make all | grep --color -E 'is up to date'
+make all DIM=%D | grep --color -E 'is up to date'
 
 cd $MUSHY_LAYER_DIR/setupNewRun/
 # make clean
-make all | grep --color -E 'is up to date'
+make all DIM=%D | grep --color -E 'is up to date'
 
 cd $MUSHY_LAYER_DIR/postProcess/
-make all | grep --color -E 'is up to date'
+make all DIM=%D | grep --color -E 'is up to date'
 
 module unload gcc/4.8.3
 module load gcc/5.2.0
