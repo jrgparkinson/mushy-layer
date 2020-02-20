8b012c3bba270b567824d4436c81603cad455e83
Dan Martin <DFMartin@lbl.gov>
2020-02-20 11:16:45 -0800

    fix random perturbation computation to avoid pattern-matching issues

--- srcSubcycle/AMRLevelMushyLayerInit.cpp
+++ srcSubcycle/AMRLevelMushyLayerInit.cpp
@@ -2254,7 +2254,11 @@ void AMRLevelMushyLayer::addPerturbation(int a_var, Real alpha, int waveNumber, 
     // Seed the random number generator in each box separately
     if (m_opt.seedRandomPert)
     {
-      Real seed = std::time(0)*procID();
+      // need to ensure that (a) we don't multiply by zero indexes 
+      // (hence the offsets), and (b) we force a different seed for 
+      // each box. (hence the dit().datInd bit). Otherwise we see pattern
+      // matching in different boxes
+      Real seed = std::time(0)*(procID()+0.5)*(dit().datInd()+0.5);
       pout() << "Seeding random number with " << seed << " on processor " << procID() << endl;
       srand(seed);
     }
